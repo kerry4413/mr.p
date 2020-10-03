c23bad4652839e95d827d136ecd661bdd834464e
gitHub <jack940197@gmail.com>
2020-10-03 19:40:55 +0800

    Initial Commit

---
+++ mr.p/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mr.p
+//
+//  Created by User06 on 2020/10/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mr.p/mr_pApp.swift
@@ -0,0 +1,17 @@
+//
+//  mr_pApp.swift
+//  mr.p
+//
+//  Created by User06 on 2020/10/3.
+//
+
+import SwiftUI
+
+@main
+struct mr_pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
